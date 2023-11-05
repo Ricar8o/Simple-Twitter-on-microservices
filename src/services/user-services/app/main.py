ca37100420debf09139e8acb9b9998946ce1af5d
Ricar8o <ricardo.8md@gmail.com>
2023-11-04 20:31:47 -0500

    Fix main api files prefix

--- src/services/user-services/app/main.py
+++ src/services/user-services/app/main.py
@@ -5,12 +5,12 @@ from app.api.v1.api import router as api_v1_router
 
 root_path = "/users-api"
 
-app = FastAPI(root_path=root_path)
+app = FastAPI()
 
 
 @app.get(root_path)
 async def root():
     return {"message": "Hello from User Services"}
 
-app.include_router(api_v1_router, prefix="/v1")
+app.include_router(api_v1_router, prefix=root_path + "/v1")
 handler = Mangum(app)
